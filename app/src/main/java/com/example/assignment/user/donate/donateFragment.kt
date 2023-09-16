a763ef7e7e440f86566ef45a051c6bbe4237643d
LingCong <lingc-wm20@student.tarc.edu.my>
2023-09-17 04:05:49 +0800

    buttom navigation bar

---
+++ app/src/main/java/com/example/assignment/user/donate/donateFragment.kt
@@ -0,0 +1,32 @@
+package com.example.assignment.user.donate
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.assignment.R
+
+class donateFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = donateFragment()
+    }
+
+    private lateinit var viewModel: DonateViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_donate, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DonateViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
