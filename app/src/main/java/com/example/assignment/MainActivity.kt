1fbcf58b8dbc63e939e78ae84a79ec872080f46b
LingCong <lingc-wm20@student.tarc.edu.my>
2023-09-18 14:47:08 +0800

    app bar menu

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -9,29 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_home)
 
-        // Set up the BottomNavigationView
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.buttom_navigation)
 
-        // Set the initial selected item
-        bottomNavigationView.selectedItemId = R.id.home
-
-        // Set the item selected listener
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home -> {
-                    // Handle item 1 selection
-                    // Update your UI or perform actions for item 1
-                    true
-                }
-                R.id.profile -> {
-                    // Handle item 2 selection
-                    // Update your UI or perform actions for item 2
-                    true
-                }
-                // Add more cases for additional items if needed
-                else -> false
-            }
-        }
     }
 
 
